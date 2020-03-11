e1a2cc2c52453e1c402207021b27694000c0ff96
Arkobat <kr-jo@live.dk>
2020-03-11 16:49:53 +0100

    Updated Study Group command to only work in #Botspam

--- src/main/java/io/github/arkobat/softwarebot/commands/StudiegruppeCmd.java
+++ src/main/java/io/github/arkobat/softwarebot/commands/StudiegruppeCmd.java
@@ -4,8 +4,7 @@ import io.github.arkobat.softwarebot.Command;
 import io.github.arkobat.softwarebot.Settings;
 import io.github.arkobat.softwarebot.utils.NumberUtils;
 import io.github.arkobat.softwarebot.utils.Utils;
-import net.dv8tion.jda.core.entities.Category;
-import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 import java.util.Set;
@@ -17,6 +16,9 @@ public class StudiegruppeCmd extends Command {
 
     @Override
     public void execute(MessageReceivedEvent e) {
+        if (e.getChannel().getIdLong() != Settings.BOT_SPAM_CHANNEL_ID) {
+            return;
+        }
         String[] args = Utils.getArgs(e.getMessage());
         if (args.length != 3) {
             e.getChannel().sendMessage("Forkert syntax. Brug `!gruppe <linje> <nr>`").queue();
@@ -49,35 +51,19 @@ public class StudiegruppeCmd extends Command {
         });
 
         TextChannel groupChannel = cat.getTextChannels().stream().filter(c -> c.getName().equalsIgnoreCase("gruppe-" + education + "-" + group)).findFirst().orElse(null);
-        TextChannel teamChannel = cat.getTextChannels().stream().filter(c -> c.getName().equalsIgnoreCase("hold-" + education + "-" + getTeam(group))).findFirst().orElse(null);
-        if (teamChannel == null) {
-            cat.createTextChannel("hold-" + education + "-" + getTeam(group)).queue(c -> c.putPermissionOverride(e.getMember()).setAllow(1024L).queue());
-        } else {
-            teamChannel.putPermissionOverride(e.getMember()).setAllow(1024L).queue();
-        }
         if (groupChannel == null) {
-            cat.createTextChannel("gruppe-" + education + "-" + group).queue(c -> c.putPermissionOverride(e.getMember()).setAllow(1024L).queue());
+            cat.createTextChannel("gruppe-" + education + "-" + group).queue(c -> {
+                c.putPermissionOverride(e.getMember()).setAllow(1024L).queue();
+                inform(e.getMember(), (TextChannel) c, e.getChannel());
+            });
         } else {
             groupChannel.putPermissionOverride(e.getMember()).setAllow(1024L).queue();
+            inform(e.getMember(), groupChannel, e.getChannel());
         }
-        e.getChannel().sendMessage("Du er nu blevet tilføjet til team og gruppe kanalen. Find dem i bunden af Discorden").queue();
 
     }
 
-    private int getTeam(int group) {
-        if (group >= 1 && group <= 5) {
-            return 1;
-        } else if (group >= 6 && group <= 10) {
-            return 2;
-        } else if (group >= 11 && group <= 15) {
-            return 3;
-        } else if (group >= 16 && group <= 20) {
-            return 4;
-        } else if (group >= 21 && group <= 25) {
-            return 5;
-        } else if (group >= 26 && group <= 30) {
-            return 6;
-        }
-        return -1;
+    private void inform(Member member, TextChannel group, MessageChannel messageChannel) {
+        messageChannel.sendMessage(member.getAsMention() + ": Du er nu blevet tilføjet til " + group.getAsMention()).queue();
     }
 }
